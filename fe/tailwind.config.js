a730db3e3991292758bd0991f2f8eac463bc200d
kedong <kedong@yunlizhi.com>
2024-11-05 17:44:10 +0800

    add tailwind css

---
+++ fe/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // 指定 Angular 项目中 HTML 和 TS 文件的路径
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
