beed4d80b0821ab34b03e3e13565f424c13df1de
kedong <kedong@yunlizhi.com>
2024-11-06 14:55:59 +0800

    .

--- be/build.sh
+++ be/build.sh
@@ -12,28 +12,27 @@ rm -rf $output_dir
 mkdir -p $output_dir
 
 # 循环编译每个平台的二进制文件
-for platform in "${platforms[@]}"
-do
-    platform_split=(${platform//\// })
-    GOOS=${platform_split[0]}
-    GOARCH=${platform_split[1]}
-    output_name='docker-image-tools-'$GOOS'-'$GOARCH
+for platform in "${platforms[@]}"; do
+  platform_split=(${platform//\// })
+  GOOS=${platform_split[0]}
+  GOARCH=${platform_split[1]}
+  output_name='docker-image-tools-'$GOOS'-'$GOARCH
 
-    # Windows 平台需要添加 .exe 后缀
-    if [ $GOOS = "windows" ]; then
-        output_name+='.exe'
-    fi
+  # Windows 平台需要添加 .exe 后缀
+  if [ $GOOS = "windows" ]; then
+    output_name+='.exe'
+  fi
 
-    # 输出文件路径
-    output_path="$output_dir/$output_name"
+  # 输出文件路径
+  output_path="$output_dir/$output_name"
 
-    echo "Building for $GOOS/$GOARCH..."
-    env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_path ./cmd/docker-image-tool
+  echo "Building for $GOOS/$GOARCH..."
+  env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_path ./cmd/docker-image-tool
 
-    # 检查构建是否成功
-    if [ $? -ne 0 ]; then
-        echo "Failed to build for $GOOS/$GOARCH"
-    else
-        echo "Successfully built $output_name"
-    fi
+  # 检查构建是否成功
+  if [ $? -ne 0 ]; then
+    echo "Failed to build for $GOOS/$GOARCH"
+  else
+    echo "Successfully built $output_name"
+  fi
 done
