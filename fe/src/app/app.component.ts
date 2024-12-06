62448a68c0361fe4e98090f021b65b4be8b1c316
kedong <kedong@yunlizhi.com>
2024-12-06 15:34:22 +0800

    add home link

--- fe/src/app/app.component.ts
+++ fe/src/app/app.component.ts
@@ -5,21 +5,27 @@ import {
   OnInit,
   ViewChild,
 } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Subscription } from 'rxjs';
+import {
+  NavigationEnd,
+  Router,
+  RouterModule,
+  RouterOutlet,
+} from '@angular/router';
+import { filter, Subscription } from 'rxjs';
 import { NotificationComponent } from './notification/notification.component';
 import { NotificationService } from './notification/notification.service';
 import { MessageService } from './services/message.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NotificationComponent],
+  imports: [RouterModule, RouterOutlet, NotificationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements AfterViewInit, OnInit, OnDestroy {
   title = 'docker-image-puller';
   private subscription!: Subscription;
+  showHomeButton = false;
 
   @ViewChild(NotificationComponent)
   notificationComponent!: NotificationComponent;
@@ -27,6 +33,7 @@ export class AppComponent implements AfterViewInit, OnInit, OnDestroy {
   constructor(
     private notificationService: NotificationService,
     private messageService: MessageService,
+    private router: Router,
   ) {}
 
   ngOnInit() {
@@ -34,6 +41,16 @@ export class AppComponent implements AfterViewInit, OnInit, OnDestroy {
     this.subscription = this.messageService.message$.subscribe((data) => {
       this.showNotification(data.message, data.isSuccess);
     });
+
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        const uri = event.urlAfterRedirects.split('?')[0];
+        console.debug('url跳转', uri);
+
+        // 检查当前路由是否是某个指定路由
+        this.showHomeButton = this.isSpecificRoute(uri);
+      });
   }
 
   ngOnDestroy() {
@@ -46,5 +63,11 @@ export class AppComponent implements AfterViewInit, OnInit, OnDestroy {
 
   showNotification(message: string, isSuccess: boolean) {
     this.notificationService.show(message, isSuccess);
+  }
+
+  isSpecificRoute(url: string): boolean {
+    // 要显示home按钮的路由
+    const specificRoutes = ['/detail'];
+    return specificRoutes.includes(url);
   }
 }
